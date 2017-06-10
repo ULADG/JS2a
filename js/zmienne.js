a221fefa35f2109dfa87a9420d96e05c24288412
Urszula Dziewit <urszula.dziewit@inwenta.pl>
2017-06-10 11:22:44 +0200

    final

---
+++ js/zmienne.js
@@ -0,0 +1,30 @@
+'use strict';
+
+
+var imie;
+var imieDamskie = "Kaja";
+var wiek = 37;
+
+//
+var kolor = "czerwony";
+
+console.log(imie);
+//console.log(kolor);
+//console.log(wiek);
+
+//imie = "Sergio";
+//
+//imie = "imieDamskie";
+//
+//
+//console.log(imie);
+//console.log(kolor);
+//console.log(wiek);
+//
+//
+//
+//
+//
+funcion wyswietlZmienna() {
+	// var imie = 188;
+//
